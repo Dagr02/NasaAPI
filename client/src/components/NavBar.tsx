8cbab0ae322eb83b828dc3d058360717b6dbf324
Dagr02 <danieliusgr02@gmail.com>
2025-06-28 22:55:20 +0100

    mobile navbar menu

--- client/src/components/NavBar.tsx
+++ client/src/components/NavBar.tsx
@@ -1,18 +1,25 @@
-import { Navbar, NavbarBrand, NavbarContent, NavbarItem } from "@heroui/react"
+import { Navbar, NavbarBrand, NavbarContent, NavbarItem, NavbarMenu, NavbarMenuItem, NavbarMenuToggle } from "@heroui/react"
 import { EarthIcon } from "lucide-react"
 import ThemeToggle from "./ThemeToggle"
 import { Link } from "react-router-dom"
+import { useState } from "react"
 
 
 
 const NavBar: React.FC = () => {
-
+    const [isMenuOpen, setMenuOpen] = useState(false)
     return (
-        <Navbar isBordered>
-            <NavbarBrand>
-                <EarthIcon />
-                <span className="font-bold text-xl text-foreground text-glow">My App</span>
-            </NavbarBrand>
+        <Navbar isBordered className="flex flex-col sm:flex-row">
+            <NavbarContent>
+                <NavbarMenuToggle 
+                    aria-label={isMenuOpen ? "Close menu" : "Open menu"}
+                    className="sm:hidden"
+                />  
+                <NavbarBrand>
+                    <EarthIcon />
+                    <span className="font-bold text-xl text-foreground text-glow">My App</span>
+                </NavbarBrand>
+            </NavbarContent>
             <NavbarContent className="hidden sm:flex gap-10 text-foreground transition-colors duration-300" justify="center">
                 <NavbarItem>
                     <Link to="/">
@@ -35,6 +42,24 @@ const NavBar: React.FC = () => {
                     <ThemeToggle />
                 </NavbarItem>
             </NavbarContent>
+            <NavbarMenu>    
+                <NavbarMenuItem>
+                    <Link to="/">
+                        <span className="text-xl hover:text-primary"> Home </span>
+                    </Link>
+                </NavbarMenuItem>
+                <NavbarMenuItem>
+                    <Link to="/MRP">
+                        <span className="text-xl hover:text-primary"> Mars Rover Photos </span>
+                    </Link>
+                </NavbarMenuItem>
+                <NavbarMenuItem>
+                    <Link to="/NeoWs">
+                        <span className="text-xl hover:text-primary"> Near Earth Objects </span>
+                    </Link>
+                </NavbarMenuItem>
+            </NavbarMenu>
+
         </Navbar>
     )
 }
